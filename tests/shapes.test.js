427de05d864597afb935dcf9fead1e8fdfcb5419
Angie <angelam202@hotmail.com>
2023-12-30 23:01:40 -0600

    work on shape.test.js file

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,25 +1,22 @@
-const { Triangle, Square, Circle, Text } = require("./lib/shapes");
+const { Triangle, Square, Circle, Text } = require("../lib/shapes");
 
-test("Testing Triangle class", () => {
-  let shape = new Triangle();
-  shape.setColor("blue");
-  expect(shape.render()).toEqual(
-    '<polygon points="150,10 280,150 20,150" fill="blue" />'
-  );
+test("Test triangle shape rendering with blue color", () => {
+  const shape = new Triangle();
+  const color =('blue')
+  shape.setColor(color);
+  expect(shape.render()).toEqual(`<polygon points="150,10 280,150 20,150" fill="${color}"/>`);
 });
 
-test("Testing Square class", () => {
-  shape = new Square();
-  shape.setColor("red");
-  expect(shape.render()).toEqual(
-    '<rect x="75" y="30" width="150" height="150" fill="red" />'
-  );
+test("Test square shape rendering with red color", () => {
+  const shape = new Square();
+  const color =('red')
+  shape.setColor(color);
+  expect(shape.render()).toEqual(`<rect x="75" y="30" width="150" height="150" fill="${color}"/>`);
 });
 
-test("Testing Circle class", () => {
-  shape = new Circle();
-  shape.setColor("green");
-  expect(shape.render()).toEqual(
-    '<circle cx="150" cy="100" r="80" fill="green" />'
-  );
+test("Test circle shape rendering with green color", () => {
+  const shape = new Circle();
+  const color =('green')
+  shape.setColor(color);
+  expect(shape.render()).toEqual(`<circle cx="150" cy="100" r="80" fill="${color}"/>`);
 });
